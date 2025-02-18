08e0ee69d546bc7db0db5dd9ec5da67266abbb28
bytswe <189936361+bytswe@users.noreply.github.com>
2025-02-18 13:20:45 +0300

    Add navbar-component

---
+++ navbar-component/js/index.js
@@ -0,0 +1,10 @@
+// Write custom JavaScript here.
+// You may ignore this file and delete if if JavaScript is not required for your challenge.
+document.getElementById('mobile-menu-button').addEventListener('click', () => {
+    document.getElementById('mobile-menu').classList.toggle('translate-x-0')
+
+})
+
+document.getElementById('close-btn').addEventListener('click', () => {
+    document.getElementById('mobile-menu').classList.toggle('translate-x-0')
+})
