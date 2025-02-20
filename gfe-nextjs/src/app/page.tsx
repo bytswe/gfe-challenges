30eefbe21e8f96213dfd3f4961d042fe20a498e7
bytswe <189936361+bytswe@users.noreply.github.com>
2025-02-21 00:59:46 +0300

    Change folder name for nextjs app

---
+++ gfe-nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+      <h1>
+      Solutions for Greatfrontend Challenges by
+      <a href="https://www.greatfrontend.com/projects/u/bytswe"
+        ><b>bytswe</b></a
+      >
+    </h1>
+  )
+}
